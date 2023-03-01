a315b8bb27416c965b11d50e53530c0df3da7eed
mmmmbmmmm <95270216+mmmmbmmmm@users.noreply.github.com>
2023-03-01 13:36:37 +0300

    Update Сourier.kt

--- src/main/kotlin/Сourier.kt
+++ src/main/kotlin/Сourier.kt
@@ -1,2 +1,4 @@
-class Сourier {
+data class Courier(var name: String, var courierTitle : String)//класс курьер , к которм определяеться какие аргументы будут у курьера
+{
+    var id: Int? = null
 }
